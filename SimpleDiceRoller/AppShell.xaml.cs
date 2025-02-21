1e19b33843ff29434a05f801e8afbcfd1e77f0b1
digydigdogdead <carlyle.laurent@gmail.com>
2025-02-21 15:37:28 +0000

    Add project files.

---
+++ SimpleDiceRoller/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleDiceRoller
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimpleDiceRoller/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleDiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
