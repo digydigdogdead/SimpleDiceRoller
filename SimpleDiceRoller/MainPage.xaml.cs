f2168d4c5af3bcdf7ce2ad8d5ce731bfcc24683d
digydigdogdead <carlyle.laurent@gmail.com>
2025-02-26 11:58:16 +0000

    Add logic to GenerateDie method that recognises which die button invoked it. Correct name of methods

--- SimpleDiceRoller/MainPage.xaml.cs
+++ SimpleDiceRoller/MainPage.xaml.cs
@@ -9,12 +9,23 @@
             InitializeComponent();
         }
 
-        public void GenerateDice(object sender, EventArgs e)
+        public void GenerateDie(object sender, EventArgs e)
         {
+            int maxValue;
 
+            if (sender == btn_d2) maxValue = 2;
+            else if (sender == btn_d4) maxValue = 4;
+            else if (sender == btn_d6) maxValue = 6;
+            else if (sender == btn_d8) maxValue = 8;
+            else if (sender == btn_d10) maxValue = 10;
+            else if (sender == btn_d12) maxValue = 12;
+            else if (sender == btn_d20) maxValue = 20;
+            else maxValue = 100;
+
+            Console.WriteLine(maxValue);
         }
 
-        public void MakeCustomDice(object sender, EventArgs e)
+        public void MakeCustomDie(object sender, EventArgs e)
         {
 
         }
