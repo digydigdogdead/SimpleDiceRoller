1089fdff26db09e5babddc51422571a4438eecca
digydigdogdead <carlyle.laurent@gmail.com>
2025-03-13 20:09:30 +0000

    Add RollValue and dName bindable properties to Die XAML and codebehind

--- SimpleDiceRoller/CustomViews/Die.xaml.cs
+++ SimpleDiceRoller/CustomViews/Die.xaml.cs
@@ -6,4 +6,22 @@ public partial class Die : ContentView
 	{
 		InitializeComponent();
 	}
+
+	public static readonly BindableProperty dNameProperty =
+		BindableProperty.Create("dName", typeof(string), typeof(Die));
+
+	public string dName
+	{
+		get => (string)GetValue(dNameProperty);
+		set => SetValue(dNameProperty, value);
+	}
+
+	public static readonly BindableProperty RollValueProperty =
+		BindableProperty.Create("RollValue", typeof(string), typeof(Die));
+
+	public string RollValue
+	{
+		get => (string)GetValue(RollValueProperty);
+		set => SetValue(RollValueProperty, value);
+	}
 }
